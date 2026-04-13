e61737c6173360630385564a691bb9fa6dc6e951
vicenteedu <vicentelemesens@gmail.com>
2026-04-13 15:22:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Beecrowd1001
+
+Desenvolvido utilizado JDK 22.0.1 e intellij 2026.1
+
+
+
+
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+   static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, X;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        X = A + B;
+
+        System.out.println("SOMA = " + X);
+
+
+
+    }}
